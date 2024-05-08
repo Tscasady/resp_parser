ac6a02b627104fd3b4e0199907114582b8b43464
Thomas Casady <tscasady@gmail.com>
2024-05-07 19:49:46 -0500

    impl parse int, simple_error, add basic tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,24 @@
-use nom::{bytes::complete::{tag, take_until, take_while}, character::complete::one_of, sequence::terminated, IResult};
+use nom::{
+    bytes::complete::{tag, take_while},
+    character::complete::{i64, one_of},
+    combinator::eof,
+    sequence::terminated,
+    IResult,
+};
 
 //TODO: Map / sets might have to be separate.
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum RespType<'a> {
     SString(&'a str),
     BString,
-    SError,
+    SError(&'a str),
     BError,
     Array,
     Null,
     Bool,
     Double,
     BigNum,
-    Int,
+    Int(i64),
     VString,
     Map,
     Set,
@@ -21,20 +27,12 @@ pub enum RespType<'a> {
 
 pub fn parse(input: &str) -> IResult<&str, RespType> {
     let (input, resp_type) = one_of("+*-:$_#,(=%!~>")(input)?;
-    match resp_type {
-        '+' => parse_simple_string(input),
-        '*' => {
-            todo!()
-        }
-        '-' => {
-            todo!()
-        }
-        ':' => {
-            todo!()
-        }
-        '$' => {
-            parse_bulk_string(input)
-        }
+    let parser = match resp_type {
+        '+' => parse_simple_string,
+        '*' => todo!(),
+        '-' => parse_simple_error,
+        ':' => parse_int,
+        '$' => parse_bulk_string,
         '_' => {
             todo!()
         }
@@ -66,13 +64,26 @@ pub fn parse(input: &str) -> IResult<&str, RespType> {
             todo!()
         }
         _ => unreachable!(),
-    }
+    };
+    terminated(parser, eof)(input)
 }
 
 ///Parses slice into Simple String.
 fn parse_simple_string(input: &str) -> IResult<&str, RespType> {
-    let (input, value) = terminated(take_while(|c| c != '\r' && c != '\n'), crlf)(input)?;
+    let (input, value) = parse_simple_string_raw(input)?;
     Ok((input, RespType::SString(value)))
+}
+
+///Parses slice into Simple Error.
+///Simple Errors are Simple Strings but prefixed with a '-'.
+///By convetion they include an error type, but the parser doesn't check for this type.
+fn parse_simple_error(input: &str) -> IResult<&str, RespType> {
+    let (input, value) = parse_simple_string_raw(input)?;
+    Ok((input, RespType::SError(value)))
+}
+
+fn parse_simple_string_raw(input: &str) -> IResult<&str, &str> {
+    terminated(take_while(|c| c != '\r' && c != '\n'), crlf)(input)
 }
 
 ///CRLF tag helper
@@ -80,12 +91,32 @@ fn crlf(input: &str) -> IResult<&str, &str> {
     tag("\r\n")(input)
 }
 
-fn parse_bulk_string(input: &str) -> IResult<&str, RespType> {
-    //cant contain clrf before end
-    //must end wit clrf
-    //must be correct len?
-    // Ok((input, RespType::BString(value)))
-    todo!()
-
+fn parse_int(input: &str) -> IResult<&str, RespType> {
+    let (input, value) = terminated(i64, crlf)(input)?;
+    Ok((input, RespType::Int(value)))
 }
 
+fn parse_bulk_string(input: &str) -> IResult<&str, RespType> {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn parse_simple_string_test() {
+        let input = "Ok\r\n";
+        assert_eq!(
+            parse_simple_string(input).unwrap().1,
+            RespType::SString("Ok")
+        );
+    }
+
+    #[test]
+    fn parse_i64_test() {
+        let input = ":-21\r\n";
+        assert_eq!(parse(input).unwrap().1, RespType::Int(-21));
+    }
+}
