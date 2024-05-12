329fd3c7f22ef850e7b12513cdefa022dfa432f5
Thomas Casady <tscasady@gmail.com>
2024-05-12 15:45:56 -0500

    Add get and set cmds.

--- src/resp_type.rs
+++ src/resp_type.rs
@@ -22,8 +22,8 @@ pub enum RespType<'a> {
 pub enum Command<'a> {
     Ping,
     Echo { args: Vec<RespType<'a>> },
-    Set,
-    Get,
+    Set { args: Vec<RespType<'a>>},
+    Get { args: Vec<RespType<'a>>},
 }
 
 impl<'a> RespType<'a> {
@@ -52,22 +52,3 @@ impl<'a> RespType<'a> {
     }
 }
 
-
-// pub fn to_command<'a>(value: RespType<'a>) -> Result<Command<'a>, String> {
-//     match value {
-//         RespType::Array(args) => match args[0] {
-//             RespType::BString(command) => match command.to_lowercase().as_str() {
-//                 "ping" => Ok(Command::Ping),
-//                 "echo" => Ok(Command::Echo {
-//                     args: args[1..].to_vec(),
-//                 }),
-//                 _ => panic!("Not a known command: {}", command),
-//             },
-//             _ => panic!(
-//                 "First element of Array must be a bulk String in order to be a command. Given {:?}",
-//                 args[0]
-//             ),
-//         },
-//         _ => panic!("Only RespType::Array can be converted to commands."),
-//     }
-// }
